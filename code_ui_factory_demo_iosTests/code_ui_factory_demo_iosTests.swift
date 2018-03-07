fe4c50dc36d202cef2207b005c0375de2d9965e2
rov <valadares.thel@gmail.com>
2018-03-07 08:32:50 -0300

    First commit

---
+++ code_ui_factory_demo_iosTests/code_ui_factory_demo_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  code_ui_factory_demo_iosTests.swift
+//  code_ui_factory_demo_iosTests
+//
+//  Created by Raoni Valadares on 07/03/18.
+//  Copyright © 2018 solo. All rights reserved.
+//
+
+import XCTest
+@testable import code_ui_factory_demo_ios
+
+class code_ui_factory_demo_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
